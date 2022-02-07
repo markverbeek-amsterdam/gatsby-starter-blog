821dad6fc46c938f8debc56388adbd55c8a48dab
Mark Verbeek <markverbeek@mac.com>
2022-02-07 08:13:36 +0100

    title update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `It's digital test site`,
     author: {
       name: `Kyle Mathews`,
       summary: `who lives and works in San Francisco building useful things.`,
