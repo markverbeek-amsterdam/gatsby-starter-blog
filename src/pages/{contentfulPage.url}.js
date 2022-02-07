fc528af8c09b9a8eb698c62136344fca65556c34
Mark Verbeek <markverbeek@mac.com>
2022-02-07 11:46:22 +0100

    contentful page

--- src/pages/{contentfulPage.url}.js
+++ src/pages/{contentfulPage.url}.js
@@ -1,26 +1,26 @@
 import React from "react"
 import { graphql } from "gatsby"
 
-// const Page = ({ data }) => {
-//   return (
-//     <main>
-//       <h1>{data.contentfulPage.title}</h1>
-//       <p>{data.contentfulPage.description.description}</p>
-//       <img
-//         src={data.contentfulPage.image.file.url}
-//         alt={data.title}
-//         style={{
-//           width: 300,
-//           height: "auto",
-//         }}
-//       />
-//     </main>
-//   )
-// }
-
 const Page = ({ data }) => {
-  return <pre>{JSON.stringify(data, null, 2)}</pre>
+  return (
+    <main>
+      <h1>{data.contentfulPage.title}</h1>
+      <p>{data.contentfulPage.description.description}</p>
+      {/* <img
+        src={data.contentfulPage.image.file.url}
+        alt={data.title}
+        style={{
+          width: 300,
+          height: "auto",
+        }}
+      /> */}
+    </main>
+  )
 }
+
+// const Page = ({ data }) => {
+//   return <pre>{JSON.stringify(data, null, 2)}</pre>
+// }
 
 export const data = graphql`
   query pageQuery($id: String) {
